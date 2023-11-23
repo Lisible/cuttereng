286bf4f5ad573c5f91672f86fe6de452e11cb7c0
Clement Sibille <clements+git@lisible.xyz>
2023-11-23 23:25:41 +0900

    Fix variadic arguments handling in log

--- cuttereng/src/log.c
+++ cuttereng/src/log.c
@@ -2,12 +2,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void log_message(log_level_t log_level, const char *filename, int line_number,
-                 const char *format_str, ...) {
+void log_message(const char *format_str, ...) {
   va_list args;
   va_start(args, format_str);
-  fprintf(stderr, format_str, log_level_to_string(log_level), filename,
-          line_number, args);
+  vfprintf(stderr, format_str, args);
   va_end(args);
 }
 
@@ -15,7 +13,7 @@ const char *log_level_to_string(log_level_t log_level) {
   switch (log_level) {
   case TRACE:
     return "TRACE";
-  case DEBUG:
+  case DBG:
     return "DEBUG";
   case WARN:
     return "WARN";
--- cuttereng/src/log.h
+++ cuttereng/src/log.h
@@ -4,23 +4,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-typedef enum log_level_t {
-	TRACE,
-	DEBUG,
-	WARN,
-	ERROR,
-	INFO
-} log_level_t;
+typedef enum log_level_t { TRACE, DBG, WARN, ERROR, INFO } log_level_t;
 
 #define LOG_TRACE(format_str, ...) LOG(TRACE, format_str, ##__VA_ARGS__)
-#define LOG_DEBUG(format_str, ...) LOG(DEBUG, format_str, ##__VA_ARGS__)
+#define LOG_DEBUG(format_str, ...) LOG(DBG, format_str, ##__VA_ARGS__)
 #define LOG_WARN(format_str, ...) LOG(WARN, format_str, ##__VA_ARGS__)
 #define LOG_ERROR(format_str, ...) LOG(ERROR, format_str, ##__VA_ARGS__)
 #define LOG_INFO(format_str, ...) LOG(INFO, format_str, ##__VA_ARGS__)
-#define LOG(log_level, format_str, ...) \
-	do { log_message(log_level, __FILE__, __LINE__, "[%s %s:%d] " format_str "\n", ##__VA_ARGS__); } while(0)
+#define LOG(log_level, format_str, ...)                                        \
+  do {                                                                         \
+    log_message("[%s %s:%d] " format_str "\n", log_level_to_string(log_level), \
+                __FILE__, __LINE__, ##__VA_ARGS__);                            \
+  } while (0)
 
-void log_message(log_level_t log_level, const char* filename, int line_number, const char* format_str, ...);
-const char* log_level_to_string(log_level_t log_level);
+void log_message(const char *format_str, ...);
+const char *log_level_to_string(log_level_t log_level);
 
 #endif // CUTTERENG_LOG_H
