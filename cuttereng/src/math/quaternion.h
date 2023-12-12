0850c04731dc57b8dba8a9241e19623f294e4dce
Clement Sibille <clements+git@lisible.xyz>
2023-12-12 17:49:39 +0900

    Add quaternion module

---
+++ cuttereng/src/math/quaternion.h
@@ -0,0 +1,10 @@
+#ifndef CUTTERENG_MATH_QUATERNION_H
+#define CUTTERENG_MATH_QUATERNION_H
+#include "vector.h"
+
+typedef struct {
+  float scalar_part;
+  v3f vector_part;
+} Quaternion;
+
+#endif // CUTTERENG_MATH_QUATERNION_H
