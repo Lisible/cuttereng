1e0e3df03b5fa8ddf60bbe07416bfe7df32af768
Clement Sibille <clements+git@lisible.xyz>
2023-12-21 16:58:19 +0900

    Add vec module for defining dynamic arrays

---
+++ cuttereng/src/vec.h
@@ -0,0 +1,70 @@
+#ifndef CUTTERENG_VEC_H
+#define CUTTERENG_VEC_H
+
+#include "common.h"
+#include "memory.h"
+#include <string.h>
+
+#define DECL_VEC(T, name)                                                      \
+  typedef struct {                                                             \
+    T *data;                                                                   \
+    size_t length;                                                             \
+    size_t capacity;                                                           \
+  } name;                                                                      \
+  void name##_init(name *vec);                                                 \
+  void name##_deinit(name *vec);                                               \
+  void name##_reserve(name *vec, size_t length_to_reserve);                    \
+  void name##_push_back(name *vec, T value);                                   \
+  void name##_append(name *vec, T *values, size_t count);                      \
+  size_t name##_length(name *vec);                                             \
+  size_t name##_capacity(name *vec);
+
+#define DEF_VEC(T, name, initial_capacity)                                     \
+  void name##_init(name *vec) {                                                \
+    vec->capacity = initial_capacity;                                          \
+    vec->data = memory_allocate(vec->capacity * sizeof(T));                    \
+    vec->length = 0;                                                           \
+  }                                                                            \
+  void name##_deinit(name *vec) {                                              \
+    memory_free(vec->data);                                                    \
+    vec->data = NULL;                                                          \
+    vec->capacity = 0;                                                         \
+    vec->length = 0;                                                           \
+  }                                                                            \
+  void name##_reserve(name *vec, size_t length_to_reserve) {                   \
+    if (vec->length + length_to_reserve < vec->capacity) {                     \
+      /* No need to reallocate as there is still enough capacity */            \
+      return;                                                                  \
+    }                                                                          \
+                                                                               \
+    size_t new_capacity = vec->capacity * 2;                                   \
+    if (vec->length + length_to_reserve > new_capacity) {                      \
+      new_capacity = vec->length + length_to_reserve;                          \
+    }                                                                          \
+                                                                               \
+    vec->data = memory_reallocate(vec->data, new_capacity * sizeof(T));        \
+    if (!vec->data) {                                                          \
+      /* TODO maybe improve that error handling? */                            \
+      LOG_ERROR("Vec reallocation failed, no memory left");                    \
+      abort();                                                                 \
+    }                                                                          \
+    vec->capacity = new_capacity;                                              \
+  }                                                                            \
+  void name##_push_back(name *vec, T value) {                                  \
+    intvec_reserve(vec, vec->length + 1);                                      \
+    vec->data[vec->length] = value;                                            \
+    vec->length++;                                                             \
+  }                                                                            \
+  void name##_append(name *vec, T *values, size_t count) {                     \
+    intvec_reserve(vec, vec->length + count);                                  \
+    memcpy(vec->data + vec->length, values, count * sizeof(T));                \
+    vec->length += count;                                                      \
+  }                                                                            \
+  size_t name##_length(name *vec) { return vec->length; }                      \
+  size_t name##_capacity(name *vec) { return vec->capacity; }
+
+#define VEC_IMPL(T, name, initial_capacity)                                    \
+  DECL_VEC(T, name)                                                            \
+  DEF_VEC(T, name, initial_capacity)
+
+#endif // CUTTERENG_VEC_H
---
+++ cuttereng/tests/vec.c
@@ -0,0 +1,44 @@
+#include "test.h"
+#include <log.h>
+#include <memory.h>
+#include <vec.h>
+
+VEC_IMPL(int, intvec, 128);
+
+void t_vec_init() {
+  intvec vec = {0};
+  intvec_init(&vec);
+  ASSERT(intvec_length(&vec) == 0);
+  ASSERT(intvec_capacity(&vec) == 128);
+  intvec_deinit(&vec);
+}
+
+void t_vec_push_back() {
+  intvec vec = {0};
+  intvec_init(&vec);
+  ASSERT(intvec_length(&vec) == 0);
+  intvec_push_back(&vec, 10);
+  ASSERT(intvec_length(&vec) == 1);
+  intvec_push_back(&vec, 13);
+  intvec_push_back(&vec, 15);
+  ASSERT(intvec_length(&vec) == 3);
+  ASSERT(vec.data[0] == 10);
+  ASSERT(vec.data[1] == 13);
+  ASSERT(vec.data[2] == 15);
+  intvec_deinit(&vec);
+}
+void t_vec_append() {
+  intvec vec = {0};
+  intvec_init(&vec);
+  ASSERT(intvec_length(&vec) == 0);
+  intvec_push_back(&vec, 10);
+  intvec_append(&vec, (int[]){11, 12, 13}, 3);
+  ASSERT(intvec_length(&vec) == 4);
+  ASSERT(vec.data[0] == 10);
+  ASSERT(vec.data[1] == 11);
+  ASSERT(vec.data[2] == 12);
+  ASSERT(vec.data[3] == 13);
+  intvec_deinit(&vec);
+}
+
+TEST_SUITE(TEST(t_vec_init), TEST(t_vec_push_back), TEST(t_vec_append))
